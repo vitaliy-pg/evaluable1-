23767c3abeff24c81a751ede91f95851ce98045c
vitaliy-pg <aljndrvt@myuax.com>
2024-10-09 22:14:03 +0200

    empiezo el ejercicio 8 haciendo la estructura del array

---
+++ ejercicio8.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+typedef struct {
+    int arrInt[10];   // Array de enteros positivos o -1 si no hay valores
+    int arrSize;      // Número de elementos almacenados
+    int arrAdd;       // Suma de los elementos del array
+} arrayLength_t;
