3b6b9a5d7a09d76a8de06f9c924ef34d217ca045
vitaliy-pg <aljndrvt@myuax.com>
2024-10-09 20:34:34 +0200

    inicio la funcion de los arrays

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,5 +1,23 @@
+#include <stdio.h>
+
+
 typedef struct {
     int arrInt[10];   // Array de enteros positivos o -1 si no hay valores
     int arrSize;      // Número de elementos almacenados
     int arrAdd;       // Suma de los elementos del array
 } arrayLength_t;
+
+int initArray(arrayLength_t *arr) {
+    if (arr == NULL) {
+        return -1; // Error si el puntero es nulo
+    }
+
+    for (int i = 0; i < 10; i++) {
+        arr->arrInt[i] = -1;
+    }
+
+    arr->arrSize = 0;
+    arr->arrAdd = 0;
+
+    return 0; // Todo salió bien
+}
