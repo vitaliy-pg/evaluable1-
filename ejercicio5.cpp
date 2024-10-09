aef705140343b629de300f7b8e2b0910ca13f1c0
vitaliy-pg <aljndrvt@myuax.com>
2024-10-09 21:02:28 +0200

    funcion para iniciar el array

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -6,5 +6,18 @@ typedef struct {
     int arrAdd;       // Suma de los elementos del array
 } arrayLength_t;
 
-// Created by vital on 09/10/2024.
-//
+int initArray(arrayLength_t *arr) {
+    if (arr == NULL) {
+        return -1; // Error si el puntero es nulo
+    }
+
+    // Inicializar los valores del array a -1
+    for (int i = 0; i < 10; i++) {
+        arr->arrInt[i] = -1;
+    }
+
+    arr->arrSize = 0;
+    arr->arrAdd = 0;
+
+    return 0; // Éxito
+}
