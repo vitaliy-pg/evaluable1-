0a30937c24d75c1b64dcf513d8420ada40dbda06
vitaliy-pg <aljndrvt@myuax.com>
2024-10-09 20:23:30 +0200

    funcion para añadir un nuevo elemento al array

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -11,3 +11,17 @@ void inicializar(arrayLength_t *arr) {
     arr->arrSize = 0;
     arr->arrAdd = 0;
 }
+void agregarElemento(arrayLength_t *arr, int valor) {
+    if (valor <= 0) {
+        printf("El valor debe ser un entero positivo.\n");
+        return;
+    }
+
+    if (arr->arrSize < 10) {
+        arr->arrInt[arr->arrSize] = valor;
+        arr->arrSize++;
+        arr->arrAdd += valor;
+    } else {
+        printf("El array está lleno.\n");
+    }
+}
