dfadece64589aa3418fd39b22e59f20528b29ae6
vitaliy-pg <aljndrvt@myuax.com>
2024-10-09 21:01:54 +0200

    empezamos el ejercicio 5 declarando la estructura del array

---
+++ ejercicio5.cpp
@@ -0,0 +1,10 @@
+#include<studio.h>
+
+typedef struct {
+    int arrInt[10];   // Array de enteros positivos o -1 si no hay valores
+    int arrSize;      // Número de elementos almacenados
+    int arrAdd;       // Suma de los elementos del array
+} arrayLength_t;
+
+// Created by vital on 09/10/2024.
+//
